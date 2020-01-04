d92905095ab4e01bf532c3ad5176871ad7332bb6
Christian Marc Caballero <mez.codedev@gmail.com>
2020-01-04 12:24:35 -0500

    global variables declared -- no functions

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,22 @@
-let userInput;
+const userPrompt = require(`readline=sync`);
+const userInput = false;
+
+const deck = [];
+const num = [
+  "Ace",
+  "King",
+  "Queen",
+  "Jack",
+  "10",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2"
+];
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 
 function initPlayers() {}
