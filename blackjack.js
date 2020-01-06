bb1ba3f5e6ce45fd3a912e9e6e2c4df16e3959c8
Devmez <mez.codedev@gmail.com>
2020-01-06 03:34:35 -0500

    added a deck shuffler

--- blackjack.js
+++ blackjack.js
@@ -3,29 +3,17 @@ let userInputWord = false;
 let userInputNum = false;
 let userInputYesNo = false;
 let playerDetail = {};
+let playerNum = 0;
 
 let deck = [];
-const num = [
-  "Ace",
-  "King",
-  "Queen",
-  "Jack",
-  "10",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2"
-];
-const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+let shuffledDeck = 
+
+const suits = ["of Spades", "of Hearts", "of Diamonds", "of Clubs"];
+const num = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
 
 initPlayers();
 
 function initPlayers() {
-    let playerNum = 0;
     userInputNum = userPrompt.question(`How many players would like to play?: `);
 
     if (userInputNum > 0) {
@@ -35,7 +23,7 @@ function initPlayers() {
             playerName();
             function playerName() {
                 userInputWord = userPrompt.question(`[Player ` + playerNum + `]` + ` Please input your display name: `);
-                playerDetail['Player '+ playerNum] = {name: userInputWord, cards: []};
+                playerDetail['Player '+ playerNum] = {name: userInputWord, hand: []};
 
                 if (!userInputWord) {
                     console.log(`Invalid input. Try again.`)
@@ -46,15 +34,15 @@ function initPlayers() {
                 };
             };
         };
-    
-        console.log(playerDetail);
-        
+
         userInputYesNo = userPrompt.question(`Ready to start? [y / n]: `.toLowerCase());
-        if (userInputYesNo == 'y'){
-            initDeck(); 
-        } else if (userInputYesNo == 'n'){
-            
-        }
+
+        if (userInputYesNo == 'y') {
+            initDeck();
+            userInputYesNo = false;
+        } else if (userInputYesNo == 'n') {
+            console.log(`NOT YET CODED IN`);
+        };
 
     } else if (!userInputNum) {
         console.log(`Invalid input.`);
@@ -64,3 +52,31 @@ function initPlayers() {
         initPlayers();
     };
 };
+
+function initDeck() {
+    for (let i = 0; i < num.length; i++) {
+        for (let a = 0; a < suits.length; a++) {
+            deck.push(num[i] + " " + suits[a]);
+        };
+    };
+
+    prepareCardShoe(deck);
+};
+
+function prepareCardShoe(shuflDeck) {
+    let randomPos;
+    let card;
+
+    for (x = shuflDeck.length - 1; x > 0; x --) {
+        randomPos = Math.floor(Math.random() * (x + 1));
+        card = shuflDeck[x];
+        shuflDeck[x] = shuflDeck[randomPos];
+        shuflDeck[randomPos] = card;
+    };
+
+    return shuflDeck;
+};
+
+function initGame() {
+    
+}
